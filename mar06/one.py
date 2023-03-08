09e9c61f5a1a9cb04a17a7e220502882480d618e
bm-raja <bm-raja@users.noreply.github.com>
2023-03-08 14:50:23 +0100

    Mar 06

---
+++ mar06/one.py
@@ -0,0 +1,16 @@
+name = 'Alice'
+age = 34
+hight = 1.68
+active = True
+
+values = [1, 2, 3, 4, 5]
+more_values = (1, 2, 3, 4, 5)
+
+print(type(name),(age),(hight),(active))
+
+things = {
+    'name' : 'Anna',
+    'age' : 23
+}
+
+print(things['age'])
